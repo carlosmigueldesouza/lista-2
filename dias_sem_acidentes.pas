70cc8b9c142a52fd3aeb641177c244c16d3b859b
CARLOS MIGUEL DE SOUZA <carlossouza2304@gmail.com>
2025-03-24 20:27:22 -0300

    Update dias_sem_acidentes.pas

--- dias_sem_acidentes.pas
+++ dias_sem_acidentes.pas
@@ -1,12 +1,13 @@
 Program dias_sem_acidentes;
-var dia, mes, anos, tempo:real;
+var dia, mes, anos, tempo:integer;
 Begin
 	write('Me informe quantos dias estamos sem acidentes');
 	read(dia);
-	mes:=(dia/30);
-	anos:=(dia/365);
+	anos:=(dia div 360);
+	mes:=(dia - anos *360) div 30;
+	tempo:=(dia - anos * 360) - mes*30;
 	writeln('Estamos à ' , anos:6:2 , 'ano');
-	writeln(mes:6:2, ' mese');
-	writeln(dia:6:2, ' dia');
+	writeln(mes:6:2, ' meses');
+	writeln(tempo:6:2, ' dia');
 	readkey;  	  
 End.
